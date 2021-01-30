388812c2626ac636f579ab0103bc1ec5fd18b2f8
Ikenna Omekam <iomekam@github.com>
2021-01-30 00:51:38 -0500

    oh well

--- app/controllers/shopify_controller.rb
+++ app/controllers/shopify_controller.rb
@@ -1,10 +1,5 @@
 class ShopifyController < ApplicationController
   def execute
-    body = JSON.parse(request.body.read)
-
-    order_created = OrderCreated.new(id: body["id"])
-
-
     render status: 200
   end
 end
