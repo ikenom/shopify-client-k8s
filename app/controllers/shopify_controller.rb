84d8dbc3543036ca42b1ad3f81be72d7ec907498
Ikenna Omekam <iomekam@github.com>
2021-01-30 00:41:54 -0500

    remove Hutch publish

--- app/controllers/shopify_controller.rb
+++ app/controllers/shopify_controller.rb
@@ -1,8 +1,11 @@
 class ShopifyController < ApplicationController
   def execute
     body = JSON.parse(request.body.read)
-    OrderCreated.create(total_price: body["total_price"])
-    pp "This is a comment: #{body["total_price"]}"
-    render status: 200, json: {test: "ikenna"}
+
+
+    order_created = OrderCreated.new(id: body["id"])
+
+
+    render status: 200
   end
 end
